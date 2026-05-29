9be4b6a1a2c9cf37ba9d9f009572d9ebaa6b33c9
mahr7742 <www.mahr7742@gmail.com>
2026-05-29 18:49:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -25,3 +25,15 @@ bot.sendVideo(msg.chat.id, res.data.data.play);
 bot.sendMessage(msg.chat.id,'حدث خطأ');
 }
 });
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
